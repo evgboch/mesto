99895333a3f61641ab602cc5168ab42d7c7ea57c
Evgeny Bocharov <evgbocharov@gmail.com>
2022-04-14 12:30:34 +0300

    feat: добавить возможность редактирования данных в профиле

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,29 @@
 const openPopupButton = document.querySelector(".profile__edit-button");
+
 const popup = document.querySelector(".popup");
-const closePopupButton = popup.querySelector(".popup_close-button");
+const formElement = popup.querySelector(".popup__container");
+const closePopupButton = formElement.querySelector(".popup__close-button");
+const nameInput = formElement.querySelector(".popup__name-field");
+const jobInput = formElement.querySelector(".popup__job-field");
+
+const profile = document.querySelector(".profile");
+const profileTitle = profile.querySelector(".profile__title");
+const profileSubtitle = profile.querySelector(".profile__subtitle");
 
 function popupOpenToggle() {
   popup.classList.toggle("popup_opened");
+  nameInput.setAttribute("value", profileTitle.textContent);
+  jobInput.setAttribute("value", profileSubtitle.textContent);
 }
 
 openPopupButton.addEventListener("click", popupOpenToggle);
 closePopupButton.addEventListener("click", popupOpenToggle);
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileTitle.textContent = nameInput.value;
+  profileSubtitle.textContent = jobInput.value;
+  popupOpenToggle();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
