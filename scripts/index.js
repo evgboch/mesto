c00547e5d39c73b7c2ccb6b43ecc0c4e42127322
Evgeny Bocharov <evgbocharov@gmail.com>
2022-05-01 23:46:36 +0300

    feat: добавить загрузку фотокарточек посредством js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,36 @@
+const initialCards = [
+  {
+    name: "Бостон",
+    link: "../images/boston.jpg",
+    alt: "Бостон"
+  },
+  {
+    name: "Парати",
+    link: "../images/paraty.jpg",
+    alt: "Парати"
+  },
+  {
+    name: "Большой каньон",
+    link: "../images/grand-canyon.jpg",
+    alt: "Большой каньон"
+  },
+  {
+    name: "Провинция Фарс",
+    link: "../images/fars-province.jpg",
+    alt: "Провинция Фарс"
+  },
+  {
+    name: "Мальдивы",
+    link: "../images/maldives.jpg",
+    alt: "Мальдивы"
+  },
+  {
+    name: "Мадрид",
+    link: "../images/madrid.jpg",
+    alt: "Мадрид"
+  }
+];
+
 const openPopupButton = document.querySelector(".profile__edit-button");
 
 const popup = document.querySelector(".popup");
@@ -10,6 +43,9 @@ const profile = document.querySelector(".profile");
 const profileTitle = profile.querySelector(".profile__title");
 const profileSubtitle = profile.querySelector(".profile__subtitle");
 
+const photoCardsContainer = document.querySelector(".photo-cards__grid");
+const photoCardsTemplate = document.querySelector(".photo-cards-template").content.querySelector(".photo-cards__element");
+
 function popupOpenToggle() {
   popup.classList.toggle("popup_opened");
   nameInput.setAttribute("value", profileTitle.textContent);
@@ -19,7 +55,7 @@ function popupOpenToggle() {
 openPopupButton.addEventListener("click", popupOpenToggle);
 closePopupButton.addEventListener("click", popupOpenToggle);
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
@@ -27,3 +63,24 @@ function formSubmitHandler (evt) {
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
+
+function generatePhotoCard(cardsElement) {
+  const newPhotoCard = photoCardsTemplate.cloneNode(true);
+
+  const photoCardTitle = newPhotoCard.querySelector(".photo-cards__title");
+  photoCardTitle.textContent = cardsElement.name;
+
+  const photoCardPicture = newPhotoCard.querySelector(".photo-cards__picture");
+  photoCardPicture.setAttribute("src", cardsElement.link);
+  photoCardPicture.setAttribute("alt", cardsElement.alt);
+
+  return newPhotoCard;
+}
+
+function renderPhotoCard(cardsElement) {
+  photoCardsContainer.prepend(generatePhotoCard(cardsElement));
+}
+
+initialCards.forEach((cardsElement) => {
+  renderPhotoCard(cardsElement);
+});
