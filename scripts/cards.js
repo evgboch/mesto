1f5f7e40c7e14cb716a8fb5d0227cbcb1282e724
Evgeny Bocharov <evgbocharov@gmail.com>
2022-05-02 12:55:47 +0300

    refactor: вынести массив с начальными данными в отдельный файл

---
+++ scripts/cards.js
@@ -0,0 +1,32 @@
+const initialCards = [
+  {
+    name: "Бостон",
+    link: "./images/boston.jpg",
+    alt: "Бостон"
+  },
+  {
+    name: "Парати",
+    link: "./images/paraty.jpg",
+    alt: "Парати"
+  },
+  {
+    name: "Большой каньон",
+    link: "./images/grand-canyon.jpg",
+    alt: "Большой каньон"
+  },
+  {
+    name: "Провинция Фарс",
+    link: "./images/fars-province.jpg",
+    alt: "Провинция Фарс"
+  },
+  {
+    name: "Мальдивы",
+    link: "./images/maldives.jpg",
+    alt: "Мальдивы"
+  },
+  {
+    name: "Мадрид",
+    link: "./images/madrid.jpg",
+    alt: "Мадрид"
+  }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,3 @@
-const initialCards = [
-  {
-    name: "Бостон",
-    link: "./images/boston.jpg",
-    alt: "Бостон"
-  },
-  {
-    name: "Парати",
-    link: "./images/paraty.jpg",
-    alt: "Парати"
-  },
-  {
-    name: "Большой каньон",
-    link: "./images/grand-canyon.jpg",
-    alt: "Большой каньон"
-  },
-  {
-    name: "Провинция Фарс",
-    link: "./images/fars-province.jpg",
-    alt: "Провинция Фарс"
-  },
-  {
-    name: "Мальдивы",
-    link: "./images/maldives.jpg",
-    alt: "Мальдивы"
-  },
-  {
-    name: "Мадрид",
-    link: "./images/madrid.jpg",
-    alt: "Мадрид"
-  }
-];
-
 const popup = document.querySelector(".popup");
 const formElement = popup.querySelector(".popup__container");
 const closePopupButton = formElement.querySelector(".popup__close-button");
