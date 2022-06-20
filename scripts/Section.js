653f0bb5ac72c97f772db668fc192861debce35b
Evgeny Bocharov <evgbocharov@gmail.com>
2022-06-21 02:46:02 +0300

    refactor: добавить класс Section

--- scripts/Section.js
+++ scripts/Section.js
@@ -1,13 +1,17 @@
 class Section {
   constructor({items, renderer}, containerSelector) {
-
+    this._items = items;
+    this._renderer = renderer;
+    this._container = document.querySelector(containerSelector);
   }
 
-  renderer() {
-
+  renderSection() {
+    this._items.forEach((item) => {
+      this._renderer(item);
+    });
   }
 
-  addItem() {
-
+  addItem(cardElement) {
+    this._container.prepend(cardElement);
   }
 }
