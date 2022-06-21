b02c46db7e03fb934be7bba82f06def2149c81d0
Evgeny Bocharov <evgbocharov@gmail.com>
2022-06-21 23:49:58 +0300

    refactor: добавить класс UserInfo

---
+++ scripts/UserInfo.js
@@ -0,0 +1,18 @@
+class UserInfo {
+  constructor({profileTitleSelector, profileSubtitleSelector}) {
+    this._profileTitle = document.querySelector(profileTitleSelector);
+    this._profileSubtitle = document.querySelector(profileSubtitleSelector);
+  }
+
+  getUserInfo() {
+    return {
+      name: this._profileTitle.textContent,
+      description: this._profileSubtitle.textContent
+    };
+  }
+
+  setUserInfo(userInfoObject) {
+    this._profileTitle.textContent = userInfoObject.name;
+    this._profileSubtitle.textContent = userInfoObject.description;
+  }
+}
