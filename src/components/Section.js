bb7551b4502792230df765fe36c95c71146fbc93
Evgeny Bocharov <evgbocharov@gmail.com>
2022-07-10 09:18:37 +0300

    fix: исправить порядок добавления карточек в контейнер

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,20 +1,16 @@
 export class Section {
   constructor({renderer}, containerSelector) {
-    // this._items = items;
     this.renderer = renderer;
     this._container = document.querySelector(containerSelector);
   }
 
   renderSection(items) {
-    // this._items.forEach((item) => {
-    //   this.renderer(item);
-    // });
-    items.forEach((item) => {
+    items.reverse().forEach((item) => {
       this.renderer(item);
     });
   }
 
   addItem(cardElement) {
-    this._container.append(cardElement);
+    this._container.prepend(cardElement);
   }
 }
